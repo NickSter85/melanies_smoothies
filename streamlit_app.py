20105bf835197f59048dddf8959422d0bbb92627
NickSter85 <nickyrongen@gmail.com>
2025-11-23 17:22:41 +0100

    Update streamlit_app.py
    put the import request to top of code, with other import statements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session --not needed in SniS
 from snowflake.snowpark.functions import col
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -41,8 +43,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered '+name_on_order+' !', icon="✅")
-
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
